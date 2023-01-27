8ed48566c3b63cd78d77f5f504ee006cf106395a
zen-lenovo <zennomi.kun@gmail.com>
2023-01-27 22:39:54 +0700

    translate to vietnamese

--- database/seeders/SeoSeeder.php
+++ database/seeders/SeoSeeder.php
@@ -17,56 +17,56 @@ class SeoSeeder extends Seeder
         $pages = [
             [
                 'page_slug' => 'home',
-                'title' => 'Welcome To Jobpilot',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'title' => 'Welcome To CongViecLam',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'blog',
                 'title' => 'Blog',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'pricing',
                 'title' => 'Pricing',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'login',
                 'title' => 'Login',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'register',
                 'title' => 'Register',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'about',
                 'title' => 'About',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'contact',
                 'title' => 'Contact',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'faq',
                 'title' => 'FAQ',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ],
             [
                 'page_slug' => 'terms-condition',
                 'title' => 'Terms Condition',
-                'description' => 'Jobpilot is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
+                'description' => 'CongViecLam is job portal laravel script designed to create, manage and publish jobs posts. Companies can create their profile and publish jobs posts. Candidate can apply job posts.',
                 'image' => asset('frontend/assets/images/jobpilot.png'),
             ]
         ];
